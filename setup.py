67b5e98217c6dacfcd65c962caac46ec7c057dfd
Harper <hrgrimsl@vt.edu>
2019-04-22 17:04:32 -0400

    adding setup

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from distutils.core import setup
 setup(
     name='SPOCK',
     version='0.1',
-    url='https://github.com/hrgrimsl/spock.git',
-    long_description=open('README.txt').read(),
-    long_description_content_type='text/markdown',
-    author='Harper Grimsley'
-    description = 'Specialized Psi4 OpenFermion Chemistry Kit',
-    requires_python = '>=3.6.0'
-    packages=find_packages(exclude=['*tests*', '*log*']),
+    url='https://github.com/hrgrimsl/spock',
+    long_description=open('README.md').read(),
+    author='Harper Grimsley',
+    description='Specialized Psi4 OpenFermion Chemistry Kit',
 )
 
